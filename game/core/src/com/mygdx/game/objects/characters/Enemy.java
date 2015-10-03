0727e22ba778e2cba8c6d550d716b93e9ed4ef20
2081902M <munro600@gmail.com>
2015-10-03 15:20:29 +0100

    added damage sound effect

--- game/core/src/com/mygdx/game/objects/characters/Enemy.java
+++ game/core/src/com/mygdx/game/objects/characters/Enemy.java
@@ -1,11 +1,15 @@
 package com.mygdx.game.objects.characters;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.mygdx.game.util.SpriteSheet;
 
 /**
  * Created by paul on 03/10/15.
  */
 public abstract class Enemy extends Character {
+	Sound sound = Gdx.audio.newSound(Gdx.files.internal("sound/grunt.mp3"));
     int collisionDamage;
 
     public Enemy(SpriteSheet spriteSheet, float width, float height) {
@@ -15,6 +19,7 @@ public abstract class Enemy extends Character {
 
     @Override
     public void onCollide(Player player){
+    	sound.play();
         player.decHealth(collisionDamage);
     }
 }
